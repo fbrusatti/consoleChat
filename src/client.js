66a3f31457fca6e823d763214ea8a0a3c63156be
Franco Brusatti <fbrusatti@gmail.com>
2018-04-29 14:10:12 -0300

    Improve the client in js

--- src/client.js
+++ src/client.js
@@ -1,16 +1,40 @@
 var net = require('net');
+var readline = require('readline');
+
+var HOST = '127.0.0.1';
+var PORT = 4848;
 
 var client = new net.Socket();
-client.connect(4848, '127.0.0.1', function() {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+client.connect(PORT, HOST, function() {
+  console.log('Connected...');
 });
 
 client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+ console.log('>>> ' + data);
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
+client.on('drain', function(data) {
+  console.log('drained buffer');
 });
+
+client.on('end', () => {
+  console.log('disconnected from server');
+  process.exit();
+});
+
+/* READ FROM STDIN */
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+  client.write(input);
+});
+
+
+// process.stdin.on('data', function(data) {
+//   if (data == 'close')
+//     client.close();
+//   client.write(data);
+// });
